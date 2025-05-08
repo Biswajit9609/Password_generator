2301593a5f55fed391670e584bbae1a86dbeae99
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-05-08 22:45:08 +0530

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import './App.css'
-import { useState } from 'react'
 import React from 'react'
+import { useState } from 'react'
+import './App.css'
 function App() {
-  const [length,setLength] = useState(6)
   return (
     <>
-      <div className='h-dvh w-dvw bg-gray-600 flex justify-center items-center'>
-        <div className='h-full w-full md:h-6/12 md:w-6/12 bg-gray-400 md:rounded-2xl flex flex-wrap'>
-        <div className='h-1/12 md:h-4/12 w-full flex justify-center items-center'>
-            <h1 className='text-2xl'>Password Generator</h1>
+      <div className="h-screen w-full bg-gray-950 text-white p-5 md:px-20 lg:px-32">
+        <div className="heading text-4xl font-bold mb-10">Random Password Generator</div>
+        <div className="password bg-blue-600 rounded-2xl mb-10">
+          <input className='h-12 w-full overflow-hidden rounded-2xl px-10' type="text" />
         </div>
-        <div className='h-4/12 w-full flex justify-center items-center flex-wrap '>
-            <input className='h-2/12 md:h-4/12 w-6/12 md:w-7/12 bg-gray-300 rounded-xl md:rounded-full mx-5 md:mx-10' type="text" />
-            <button className='bg-gray-800 text-white p-3 rounded-2xl w-3/12 md:w-2/12 md:h-auto h-2/12 flex items-center justify-center'>Copy</button>
+        <div className="control1 mb-10 flex flex-wrap">
+          <div className='w-6/12'>
+            <input id='length' type="range" value="" />
+            <br /> 
+            <label htmlFor="length">Length</label>
+          </div>
+          <div className='w-6/12'>
+          <button className='h-10 w-6/12 bg-indigo-600 rounded-2xl hover:bg-indigo-700'>Copy</button>
+          </div>
+          
         </div>
-        <div className='h-7/12 md:h-4/12 w-full flex justify-center items-stretch flex-wrap'>
-            <div className='h-2/12 w-full md:h-full md:w-4/12 flex py-auto'>
-                <label className='h-3/12 md:mx-5 w-4/12 md:w-auto flex justify-center items-center'>Length {length}</label>
-                <input id='range' className='h-3/12 md:w-3/12 rounded-full md:mx-3 w-4/12' min={6} max={12} value={length} onChange={(e)=> {setLength(e.target.value)}} type="range" />
-            </div>
-            <div className='h-2/12 w-full md:h-full md:w-4/12 py-auto'>
-                <label className='mx-3' htmlFor="">Character</label>
-                <input className='mx-3' type="checkbox" />
-            </div>
-            <div className='h-2/12 w-full md:h-full md:w-4/12 py-auto'>
-                <label htmlFor="">Number</label>
-                <input className='mx-3' type="checkbox" />
-            </div>
+        <div className="control2 text-2xl mb-10">
+          <input className='scale-150' type="checkbox" name="Allow Capital Letters" id="cap" />
+          <label htmlFor="cap"> Allow Capital Letters</label>
+          <br />
+          <input className='scale-150' type="checkbox" name="Allow Numbers" id="num" />
+          <label htmlFor="num"> Allow Numbers</label>
+          <br />
+          <input className='scale-150' type="checkbox" name="" id="char" />
+          <label htmlFor="char"> Allow Characters</label>
+          <br />
         </div>
+        <div className="generate flex justify-center">
+          <button className='h-15 w-10/12 bg-purple-600 rounded-full'>Generate</button>
         </div>
       </div>
     </>
